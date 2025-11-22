40ec35e535a56097f73f6a1aada5e19ae6d0aa46
Pratiksha Bhadalkar <bhadlkarpratiksha@gmail.com>
2025-11-22 13:22:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+/**🔹 Level 1 — Basic Coding Questions**/
+
+// 1️⃣ Write a function to check if a number is even or odd.
+// (Uses functions, numbers, condition)
+
+// function checkEvenOdd(num)
+// {
+    
+//     if(num%2==0){
+//         console.log( `${num} `, " IS  EVEN "  );
+//     }
+// else{
+//     console.log(`${num} `, " IS  odd " );
+//     }
+// }
+//     checkEvenOdd(4);
+// checkEvenOdd(3);
+
+// 2️⃣ Create a function that returns the sum of two numbers.
+// function  add(a,b){
+//     return a+b;
+// }
+// console.log(add(3,4))
+
+
+// 3️⃣ Take a variable name and print "Hello, <name>".
+// (Use template literals)
+
+//  let name="pratiksha";
+//  console.log(`Hello  ${name}`) ;
+
+
+//4️⃣ Create an array of 5 numbers and print the second element.
+// let arr1=[1,2,3,4,5];
+// console.log(arr1[2]);
+
+// 5️⃣ Declare a const object and change one of its properties.
+// (To check understanding of const behavior) 
+
+const  object=
+{
+name:"pratiksha",
+age:24
+};
+object.name="pooja";
+console.log(object)
